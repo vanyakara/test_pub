6aab0fb7ff89a682a2750cc87db8a508b7b503e4
vanyakara <147335690+vanyakara@users.noreply.github.com>
2024-07-11 10:50:20 +0300

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ || exit
+cd ~/test || exit
 git remote set-url origin git@github.com:vanyakara/test_pub.git
 git clone git@github.com:vanyakara/test_pub.git
 # Add any additional deployment commands here, such as restarting services
