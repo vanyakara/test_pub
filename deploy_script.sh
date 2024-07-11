50e5132a218f3dfccf1fac07ebade34a90bd369f
vanyakara <147335690+vanyakara@users.noreply.github.com>
2024-07-11 11:56:08 +0300

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,7 +5,9 @@ if [ -d "/home/asureuser/test_pub2" ];
 then 
 cd ~/test_pub2/test_pub
 git pull origin main
+sudo cp test.html /var/www/html/
 else
 git clone git@github.com:vanyakara/test_pub.git
+sudo cp ./test_pub/test.html /var/www/html/
 fi
 # Add any additional deployment commands here, such as restarting services
